8a7c634062996ade83e9a12da0494c3eb18baa24
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-20 18:19:15 +0530

    webappconfig update

--- Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,4 +1,13 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+//component scan eka danna
 public class WebAppConfig {
+    public WebAppConfig(){
+        System.out.println("WebAppConfig");
+    }
 }
