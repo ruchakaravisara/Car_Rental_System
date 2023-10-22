978ff0eafcf34c572b85b77b9f0e16b222526b5e
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-23 01:14:42 +0530

    admin index update

---
+++ Front_End/assets/controllers/adminDashboard.js
@@ -0,0 +1,14 @@
+$('#adminDashBoard').css('display','block');
+$('#adminCustomer').css('display','none');
+
+$('.DashBoard').click(function (){
+    $('#adminDashBoard').css('display','block');
+    $('#adminCustomer').css('display','none');
+
+});
+$('.Customer').click(function (){
+    $('#adminDashBoard').css('display','block');
+    $('#adminCustomer').css('display','block');
+
+
+});
