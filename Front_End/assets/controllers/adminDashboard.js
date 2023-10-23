141ffb2546478e209f03120700a12693925e28ea
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-23 19:22:25 +0530

    css update

--- Front_End/assets/controllers/adminDashboard.js
+++ Front_End/assets/controllers/adminDashboard.js
@@ -2,31 +2,34 @@ $('#adminDashBoard').css('display','block');
 $('#adminCustomer').css('display','none');
 $('#adminAddCar').css('display','none');
 $('#adminCarDetail').css('display','none');
+$('#adminCustomerReservation').css('display','none');
 
 $('.DashBoard').click(function (){
     $('#adminDashBoard').css('display','block');
     $('#adminCustomer').css('display','none');
     $('#adminAddCar').css('display','none');
     $('#adminCarDetail').css('display','none');
+    $('#adminCustomerReservation').css('display','none');
 });
 $('.Customer').click(function (){
     $('#adminDashBoard').css('display','block');
     $('#adminCustomer').css('display','block');
     $('#adminAddCar').css('display','none');
     $('#adminCarDetail').css('display','none');
-
+    $('#adminCustomerReservation').css('display','none');
 });
 $('.Cars').click(function (){
     $('#adminDashBoard').css('display','block');
     $('#adminAddCar').css('display','block');
     $('#adminCustomer').css('display','none');
     $('#adminCarDetail').css('display','none');
-
+    $('#adminCustomerReservation').css('display','none');
 });
 $('#btnCarDetail').click(function () {
     $('#adminDashBoard').css('display','block');
     $('#adminCarDetail').css('display','block');
     $('#adminAddCar').css('display','none');
     $('#adminCustomer').css('display','none');
-
+    $('#adminCustomerReservation').css('display','none');
 });
+
