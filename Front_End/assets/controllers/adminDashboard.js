049cbbf3994ace37bdade5ba70ebeddb8bc5708b
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-23 13:18:22 +0530

    css and admin js update

--- Front_End/assets/controllers/adminDashboard.js
+++ Front_End/assets/controllers/adminDashboard.js
@@ -1,14 +1,21 @@
 $('#adminDashBoard').css('display','block');
 $('#adminCustomer').css('display','none');
+$('#adminAddCar').css('display','none');
 
 $('.DashBoard').click(function (){
     $('#adminDashBoard').css('display','block');
     $('#adminCustomer').css('display','none');
-
+    $('#adminAddCar').css('display','none');
 });
 $('.Customer').click(function (){
     $('#adminDashBoard').css('display','block');
     $('#adminCustomer').css('display','block');
+    $('#adminAddCar').css('display','none');
 
+});
+$('.Cars').click(function (){
+    $('#adminDashBoard').css('display','block');
+    $('#adminAddCar').css('display','block');
+    $('#adminCustomer').css('display','none');
 
 });
