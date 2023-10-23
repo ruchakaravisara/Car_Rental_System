6ce197c93211c9cd6da3e2eda83cd8a28d2fc55c
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-23 17:03:03 +0530

    admin js update

--- Front_End/assets/controllers/adminDashboard.js
+++ Front_End/assets/controllers/adminDashboard.js
@@ -1,21 +1,32 @@
 $('#adminDashBoard').css('display','block');
 $('#adminCustomer').css('display','none');
 $('#adminAddCar').css('display','none');
+$('#adminCarDetail').css('display','none');
 
 $('.DashBoard').click(function (){
     $('#adminDashBoard').css('display','block');
     $('#adminCustomer').css('display','none');
     $('#adminAddCar').css('display','none');
+    $('#adminCarDetail').css('display','none');
 });
 $('.Customer').click(function (){
     $('#adminDashBoard').css('display','block');
     $('#adminCustomer').css('display','block');
     $('#adminAddCar').css('display','none');
+    $('#adminCarDetail').css('display','none');
 
 });
 $('.Cars').click(function (){
     $('#adminDashBoard').css('display','block');
     $('#adminAddCar').css('display','block');
+    $('#adminCustomer').css('display','none');
+    $('#adminCarDetail').css('display','none');
+
+});
+$('#btnCarDetail').click(function () {
+    $('#adminDashBoard').css('display','block');
+    $('#adminCarDetail').css('display','block');
+    $('#adminAddCar').css('display','none');
     $('#adminCustomer').css('display','none');
 
 });
