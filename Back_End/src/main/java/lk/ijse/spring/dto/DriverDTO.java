d8937d43c4cf3e212d2307dfc1c0c63a63ab88ed
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-24 20:32:04 +0530

    driverdto update

---
+++ Back_End/src/main/java/lk/ijse/spring/dto/DriverDTO.java
@@ -0,0 +1,26 @@
+package lk.ijse.spring.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class DriverDTO {
+    private String driverID;
+    private String name;
+    private String nic;
+    private String address;
+    private String drivingLicense;//wait
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dob;
+    private String status;
+}
