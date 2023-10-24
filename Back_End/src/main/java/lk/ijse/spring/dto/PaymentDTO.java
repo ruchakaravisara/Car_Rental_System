b3820605dff0d10346289e583a74d15f09011bad
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-24 22:39:33 +0530

    payment update

---
+++ Back_End/src/main/java/lk/ijse/spring/dto/PaymentDTO.java
@@ -0,0 +1,25 @@
+package lk.ijse.spring.dto;
+
+import lk.ijse.spring.entity.Rental;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class PaymentDTO {
+    private String paymentId;
+    private Rental rental;
+    private LocalDate date;
+    private double amount;
+    private double damageCost;
+    private String damageDescription;
+    private long extraMileage;
+    private double costPerExtraMileage;
+    private double driverWages;
+}
