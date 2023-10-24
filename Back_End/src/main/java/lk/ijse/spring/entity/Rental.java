8c8fb1979ed4c32ff28a53d294fbdfecf7d27ccf
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-24 18:43:31 +0530

    rental entity update

---
+++ Back_End/src/main/java/lk/ijse/spring/entity/Rental.java
@@ -0,0 +1,19 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Rental {
+    @Id
+    private String rentalId;
+}
