a340fc0ea0ac5d95a0e8cadc4f67ff1488c0dca9
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-24 18:41:44 +0530

    payment entity update

---
+++ Back_End/src/main/java/lk/ijse/spring/entity/Payment.java
@@ -0,0 +1,20 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Payment {
+    @Id
+    private String paymentId;
+
+}
