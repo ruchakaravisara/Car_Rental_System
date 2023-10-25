cfd140a27ef7d4b390c0ba068ced456f04df7ae2
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-25 22:44:11 +0530

    repo update

---
+++ Back_End/src/main/java/lk/ijse/spring/repo/ReservationRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.RentalDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepo extends JpaRepository<RentalDetail,String> {
+}
