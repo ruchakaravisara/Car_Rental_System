0a493769e1a72d715f642954ff267ab940f1d24a
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-25 16:39:16 +0530

    webappconfig update

--- Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,13 +1,28 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-//component scan eka danna
-public class WebAppConfig {
+@ComponentScan(basePackages = {"lk.ijse.spring.controllers","lk.ijse.spring.advicer"})
+public class WebAppConfig implements WebMvcConfigurer {
     public WebAppConfig(){
         System.out.println("WebAppConfig");
+    }
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
     }
 }
