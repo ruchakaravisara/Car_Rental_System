57f2d5761b18a30b2ea045390a046980c4a68040
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-25 16:54:12 +0530

    response util update

--- Back_End/src/main/java/lk/ijse/spring/advicer/AppWideExceptionHandler.java
+++ Back_End/src/main/java/lk/ijse/spring/advicer/AppWideExceptionHandler.java
@@ -1,9 +1,13 @@
 package lk.ijse.spring.advicer;
 
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @CrossOrigin
 public class AppWideExceptionHandler {
+    public ResponseUtil handleException(RuntimeException e){
+        return new ResponseUtil("500", e.getMessage(),"");
+    }
 }
