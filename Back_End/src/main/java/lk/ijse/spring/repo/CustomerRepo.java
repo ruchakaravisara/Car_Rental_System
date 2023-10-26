9f8276cfa1abc27ce7258ec1b23c7d7c9fa22fc0
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-26 20:03:58 +0530

    customerrepo update

--- Back_End/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
+++ Back_End/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
@@ -3,5 +3,12 @@ package lk.ijse.spring.repo;
 import lk.ijse.spring.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepo extends JpaRepository<Customer,String> {
+
+    Customer  findCustomerByEmailAndPassword(String email,String password);
+    Customer findCustomerByNic(String nic);
+    List<Customer> findCustomerByStatus(String status);
+    Customer findCustomerByEmail(String email);
 }
