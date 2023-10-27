c603d3f151cd20374e27739ea8427cc5bdc98ff3
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-27 19:26:46 +0530

    drivercontroller update

--- Back_End/src/main/java/lk/ijse/spring/controllers/DriverController.java
+++ Back_End/src/main/java/lk/ijse/spring/controllers/DriverController.java
@@ -6,6 +6,8 @@ import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("driver")
 @CrossOrigin
@@ -27,5 +29,10 @@ public class DriverController{
     public ResponseUtil deleteCustomer(String id){
         service.deleteDriver(id);
         return new ResponseUtil("Ok",id+" : Deleted.",null);
+    }
+    @GetMapping
+    public ResponseUtil getAllDrivers(){
+        ArrayList<DriverDTO> allDrivers = service.getAllDrivers();
+        return new ResponseUtil("Ok","Done.",allDrivers);
     }
 }
