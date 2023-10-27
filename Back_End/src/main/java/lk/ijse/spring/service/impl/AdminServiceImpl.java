adb9b5d87f5761fd9e46ce0d586b32120d9643d1
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-27 17:47:37 +0530

    adminserviceupdate

--- Back_End/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
@@ -37,7 +37,10 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public void deleteAdmin(String id) {
-
+        if (!repo.existsById(id)){
+            throw new RuntimeException("Driver "+id+" is Not Available.");
+        }
+        repo.deleteById(id);
     }
 
     @Override
