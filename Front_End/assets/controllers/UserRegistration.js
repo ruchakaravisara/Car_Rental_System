14c130e2803a7352786e9063625496172265404b
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-27 13:49:11 +0530

    user ajax and js update

--- Front_End/assets/controllers/UserRegistration.js
+++ Front_End/assets/controllers/UserRegistration.js
@@ -69,3 +69,109 @@ $("#btnSignUp").click(function () {
     });
 
 });
+var userNIC;
+var userDrivingLicense;
+var userEmail;
+let userPassword;
+let userImgL;
+let userStatus;
+$("#btnSignnUp").click(function (){
+
+    let email =  $("#txtLogEmail").val();
+    let password = $("#txtLogPassword").val();
+    $.ajax({
+        url: baseURL+"customer?email="+email+"&password="+password,
+        method: "get",
+        dataType:"json",
+        success: function (res) {
+            alert(res.message);
+            if (res.data.password==password  && res.data.email == email){
+
+                $("#txtCustomerUName").val(res.data.name);
+                $("#txtCustomerUContactNumber").val(res.data.contactNumber);
+                $("#txtCustomerUAddress").val(res.data.address);
+                userNIC=res.data.nic;
+                userDrivingLicense=res.data.name;
+                userEmail=res.data.email;
+                userPassword=res.data.password;
+                userImgL=res.data.nic;
+                userStatus=res.data.status;
+
+                $('.userNameTag').text(res.data.name);
+                $('.userEmailTag').text(res.data.email);
+                $('#UserViewCarRents').css('display','block');
+                $('#UserStore').css('display','none');
+                $('#userCarCatalogue').css('display','none');
+                $('#UserCheckOut').css('display','none');
+                $('#UserDashBoard').css('display','none');
+                $('#UserAccount').css('display','none');
+                $('#UserLoginAccount').css('display','none');
+
+
+            }
+            $("#txtLogEmail").val("");
+            $("#txtLogPassword").val("");
+
+            $(".Account").click(function (){
+                $('.userNameTag').text(res.data.name);
+                $('.userEmailTag').text(res.data.email);
+                $('#UserViewCarRents').css('display','block');
+
+                $('#UserDashBoard').css('display','none');
+                $('#UserAccount').css('display','none');
+                $('#UserLoginAccount').css('display','none');
+                $('#UserStore').css('display','none');
+                $('#userCarCatalogue').css('display','none');
+                $('#UserCheckOut').css('display','none');
+
+
+            });
+        },
+        error:function(error){
+            alert("Invalid email or password");
+        }
+    });
+});
+
+$("#customerUpdateModalShow").click(function (){
+    $("#customerUpdatePopUp").modal('show');
+
+});
+
+$("#btnUpdateCustomer").click(function (){
+    let customername=  $("#txtCustomerUName").val();
+    let customercnumber=  $("#txtCustomerUContactNumber").val();
+    let customercddress=  $("#txtCustomerUAddress").val();
+
+
+    let cutomer={
+
+        name: customername,
+        nic: userNIC,
+        address: customercddress,
+        drivingLicense: userDrivingLicense,
+        email: userEmail,
+        password: userPassword,
+        contactNumber: customercnumber,
+        imageLocation: userImgL,
+        status: userStatus
+    }
+
+    $.ajax({
+        url: baseURL+'customer',
+        method: 'put',
+        contentType:"application/json",
+        data:JSON.stringify(cutomer),
+        dataType:"json",
+        success: function (res) {
+            alert(res.message);
+        },
+        error:function (error){
+            let cause= JSON.parse(error.responseText).message;
+            alert(cause);
+        }
+
+    });
+
+    $("#customerUpdatePopUp").modal('toggle');
+});
