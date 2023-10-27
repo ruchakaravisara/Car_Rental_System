515aca9a249064889b2485bd9e21dde51c187379
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-27 19:13:33 +0530

    driverserviceimpl update

--- Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
@@ -27,7 +27,10 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public void updateDriver(DriverDTO dto) {
-
+        if (!repo.existsById(dto.getDriverID())){
+            throw new RuntimeException("Driver "+dto.getDriverID()+"is Not Available.");
+        }
+        repo.save( mapper.map(dto, Driver.class));
     }
 
     @Override
