2c25ade32cefbbc967dbb6d0c810cbed7e4a0fa2
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-27 19:04:31 +0530

    driverserviceimpl update

---
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
@@ -0,0 +1,43 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.entity.Driver;
+import lk.ijse.spring.repo.DriverRepo;
+import lk.ijse.spring.service.DriverService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+@Service
+@Transactional
+public class DriverServiceImpl implements DriverService {
+    @Autowired
+    private DriverRepo repo;
+    @Autowired
+    private ModelMapper mapper;
+    @Override
+    public void addDriver(DriverDTO dto) {
+        if (repo.existsById(dto.getDriverID())) {
+            throw new RuntimeException("Driver "+dto.getDriverID()+"Exist.");
+        }
+
+        repo.save(mapper.map(dto, Driver.class));
+    }
+
+    @Override
+    public void updateDriver(DriverDTO dto) {
+
+    }
+
+    @Override
+    public void deleteDriver(String id) {
+
+    }
+
+    @Override
+    public ArrayList<DriverDTO> getAllDrivers() {
+        return null;
+    }
+}
