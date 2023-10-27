5c6213c801f2836d49509aa7b1d451f9a2b50eaa
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-27 20:44:15 +0530

    car serviceimpl adn car contriller upate

--- Back_End/src/main/java/lk/ijse/spring/controllers/CarController.java
+++ Back_End/src/main/java/lk/ijse/spring/controllers/CarController.java
@@ -1,4 +1,24 @@
 package lk.ijse.spring.controllers;
 
+import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.service.CarService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/car")
+@CrossOrigin
 public class CarController {
+    @Autowired
+    private CarService service;
+
+    @PostMapping
+    public ResponseUtil addCar(CarDTO dto){
+        service.addCar(dto);
+        return new ResponseUtil("OK",dto.getRegistrationNumber()+ " Added.!",null);
+    }
 }
--- Back_End/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
@@ -1,18 +1,55 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.entity.Car;
+import lk.ijse.spring.repo.CarRepo;
 import lk.ijse.spring.service.CarService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 @Service
 @Transactional
 public class CarServiceImpl implements CarService {
+    @Autowired
+    private CarRepo repo;
+    @Autowired
+    private ModelMapper mapper;
     @Override
     public void addCar(CarDTO dto) {
+        if (repo.existsById(dto.getRegistrationNumber())){
+            throw new RuntimeException("Customer "+dto.getRegistrationNumber()+" Exist.");
+        }
+        try {
+            String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+            File uploadDir = new File(projectPath + "/uploads");
+            System.out.println(projectPath);
+            uploadDir.mkdir();
 
+            dto.getImg1().transferTo(new File(uploadDir.getAbsolutePath() + "/" + dto.getImg1().getOriginalFilename()));
+            dto.setFrontImageLocation("uploads/"+dto.getImg1().getOriginalFilename());
+
+            dto.getImg2().transferTo(new File(uploadDir.getAbsolutePath() + "/" + dto.getImg2().getOriginalFilename()));
+            dto.setBackImageLocation("uploads/"+dto.getImg2().getOriginalFilename());
+
+            dto.getImg3().transferTo(new File(uploadDir.getAbsolutePath() + "/" + dto.getImg3().getOriginalFilename()));
+            dto.setBgTransparentImageLocation("uploads/"+dto.getImg3().getOriginalFilename());
+
+            dto.getImg4().transferTo(new File(uploadDir.getAbsolutePath() + "/" + dto.getImg4().getOriginalFilename()));
+            dto.setSideImageLocation("uploads/"+dto.getImg4().getOriginalFilename());
+
+
+            repo.save(mapper.map(dto, Car.class));
+
+        }catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
