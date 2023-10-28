ec19fb0f55925d2f46e6bf1ea1607cd16f87f715
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-28 20:43:23 +0530

    reservationcontroller update

--- Back_End/src/main/java/lk/ijse/spring/controllers/ReservationController.java
+++ Back_End/src/main/java/lk/ijse/spring/controllers/ReservationController.java
@@ -1,4 +1,23 @@
 package lk.ijse.spring.controllers;
 
+import lk.ijse.spring.service.ReservationService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rentalDetail")
+@CrossOrigin
 public class ReservationController {
+    @Autowired
+    private ReservationService service;
+
+    @PutMapping()
+    public ResponseUtil updateReservation(@RequestParam String id, @RequestParam String status) {
+
+
+        service.updateReservation(id,status);
+        return new ResponseUtil("Ok",id+": Updated.",null);
+
+    }
 }
