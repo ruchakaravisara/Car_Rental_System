b7655bcd60c2ba2c09941447a0673a20381f1a80
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-28 22:39:37 +0530

    reservation controller update

--- Back_End/src/main/java/lk/ijse/spring/controllers/ReservationController.java
+++ Back_End/src/main/java/lk/ijse/spring/controllers/ReservationController.java
@@ -1,9 +1,13 @@
 package lk.ijse.spring.controllers;
 
+import lk.ijse.spring.dto.RentalDetail_DTO;
+import lk.ijse.spring.dto.ReservationDTO;
 import lk.ijse.spring.service.ReservationService;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/rentalDetail")
@@ -14,10 +18,17 @@ public class ReservationController {
 
     @PutMapping()
     public ResponseUtil updateReservation(@RequestParam String id, @RequestParam String status) {
-
-
         service.updateReservation(id,status);
         return new ResponseUtil("Ok",id+": Updated.",null);
-
+    }
+    @GetMapping
+    public ResponseUtil getAllReservations(){
+        ArrayList<ReservationDTO> allReservation = service.getAllReservation();
+        return new ResponseUtil("Ok","Done",allReservation);
+    }
+    @GetMapping(params = "emm")
+    public ResponseUtil getRents(String emm) {
+        ArrayList<RentalDetail_DTO> allReservationUsingEmail = service.getAllReservationUsingEmail(emm);
+        return new ResponseUtil("Ok", "RequestSend", allReservationUsingEmail);
     }
 }
