fec9709c74cc30f7c62f610b8d36b8c2b8aeec28
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-28 22:33:25 +0530

    reservation update

--- Back_End/src/main/java/lk/ijse/spring/repo/RentalRepo.java
+++ Back_End/src/main/java/lk/ijse/spring/repo/RentalRepo.java
@@ -2,6 +2,13 @@ package lk.ijse.spring.repo;
 
 import lk.ijse.spring.entity.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RentalRepo extends JpaRepository<Rental,String> {
+
+    @Query(value = "SELECT * FROM Rental  where rentalId=?1",nativeQuery = true)
+    Rental findRentUsingRentId(String customer_nic);
+
+    @Query(value = "SELECT rentalId FROM Rental  where customer_nic=?1 ORDER BY rentalId DESC LIMIT 1",nativeQuery = true)
+    String findLatestRentUsingNIC(String customer_nic);
 }
--- Back_End/src/main/java/lk/ijse/spring/service/impl/ReservationServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/ReservationServiceImpl.java
@@ -2,6 +2,8 @@ package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.RentalDetail_DTO;
 import lk.ijse.spring.dto.ReservationDTO;
+import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.entity.Rental;
 import lk.ijse.spring.entity.RentalDetail;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.repo.RentalRepo;
@@ -37,11 +39,49 @@ public class ReservationServiceImpl implements ReservationService {
 
     @Override
     public ArrayList<ReservationDTO> getAllReservation() {
-        return null;
+       ArrayList<ReservationDTO> lists = new ArrayList<>();
+       for (RentalDetail rentalDetail : repo.findAll()){
+           ReservationDTO map = mapper.map(rentalDetail, ReservationDTO.class);
+
+           Customer customer = rentalDetail.getRental().getCustomer();
+           String pickupLocation = rentalDetail.getRental().getPickupLocation();
+           String returnLocation = rentalDetail.getRental().getReturnLocation();
+
+           map.setCustomer(customer);
+           map.setPickupLocation(pickupLocation);
+           map.setReturnLocation(returnLocation);
+           map.setRentalId(rentalDetail.getRental().getRentalId());
+           map.setTotalDamageWaiverAmount(rentalDetail.getDamageWaiverAmount());
+           lists.add(map);
+       }
+       return lists;
     }
 
     @Override
     public ArrayList<RentalDetail_DTO> getAllReservationUsingEmail(String mail) {
-        return null;
+        Customer customerByEmail = customerRepo.findCustomerByEmail(mail);
+        String nic = customerByEmail.getNic();
+        System.out.println(nic);
+        String latestRentUsingNIC = rentalRepo.findLatestRentUsingNIC(nic);
+
+        ArrayList<RentalDetail_DTO> list = new ArrayList<>();
+        Rental rentUsingRentId = rentalRepo.findRentUsingRentId(latestRentUsingNIC);
+        System.out.println("v"+latestRentUsingNIC);
+        System.out.println(rentUsingRentId.getRentalId());
+
+
+        for (RentalDetail rentalDetail : repo.findAll()) {
+            if (rentalDetail.getRental().getRentalId() ==rentUsingRentId.getRentalId()){
+                RentalDetail_DTO map = mapper.map(rentalDetail, RentalDetail_DTO.class);
+                map.setReturnLocation(rentUsingRentId.getReturnLocation());
+                map.setPickupLocation(rentUsingRentId.getPickupLocation());
+                list.add(map);
+            }
+
+        }
+        System.out.println(list);
+
+
+        return list;
     }
 }
