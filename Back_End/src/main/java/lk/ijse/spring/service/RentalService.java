fa07c896fe99b708321ad98b373762cc652031ea
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-29 14:09:51 +0530

    rental service update

--- Back_End/src/main/java/lk/ijse/spring/service/RentalService.java
+++ Back_End/src/main/java/lk/ijse/spring/service/RentalService.java
@@ -1,4 +1,13 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.RentalDTO;
+import lk.ijse.spring.dto.Rental_DTO;
+
+import java.util.ArrayList;
+
 public interface RentalService {
+    void addRental(RentalDTO dto);
+    ArrayList<Rental_DTO> getAllRents();
+    String generateRentalId();
+    Rental_DTO searchRentByNIC(String nic);
 }
