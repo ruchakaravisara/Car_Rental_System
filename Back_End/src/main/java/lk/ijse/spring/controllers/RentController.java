6a0eefa02c9fb4571de93a3db4db0a2e9d536b01
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-29 18:57:37 +0530

    rental control update

--- Back_End/src/main/java/lk/ijse/spring/controllers/RentController.java
+++ Back_End/src/main/java/lk/ijse/spring/controllers/RentController.java
@@ -1,4 +1,24 @@
 package lk.ijse.spring.controllers;
 
+import lk.ijse.spring.dto.RentalDTO;
+import lk.ijse.spring.service.RentalService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rental")
+@CrossOrigin
 public class RentController {
+    @Autowired
+    private RentalService service;
+
+    @PostMapping
+    public ResponseUtil saveReservation(RentalDTO dto){
+        service.addRental(dto);
+        return new ResponseUtil("Ok",dto.getMail()+ " Reservation Added",null);
+    }
 }
